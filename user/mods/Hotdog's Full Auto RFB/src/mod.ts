6a3ace15541dc299b1941a5a23463b6dab9d8c1e
L-HOT-DOGGY-L <jason@thoughtfed.com>
2024-10-11 17:09:35 -0400

    Add files via upload

---
+++ user/mods/Hotdog's Full Auto RFB/src/mod.ts
@@ -0,0 +1,22 @@
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const LogTextColor_1 = require("C:/snapshot/project/obj/models/spt/logging/LogTextColor");
+const path_1 = __importDefault(require("path"));
+class Mod {
+    preAkiLoad(container) {
+        const logger = container.resolve("WinstonLogger");
+        logger.logWithColor("spray and pray", LogTextColor_1.LogTextColor.GREEN);
+    }
+    postDBLoad(container) {
+        const databaseServer = container.resolve("DatabaseServer");
+        const tables = databaseServer.getTables();
+        const rfb = tables.templates.items["5f2a9575926fd9352339381f"];
+        //---------------------------------------------------------------------------------------------
+        //saiga-9 changes
+        rfb._props.weapFireType.push("fullauto");
+        rfb._props.bFirerate = 600;
+    }
+}
+module.exports = { mod: new Mod() };
